e135bd7ba5e0d0cc1d323957ed90fbe2df73fd83
vigneshmathi18 <56676929+vigneshmathi18@users.noreply.github.com>
2021-06-05 20:56:10 +0530

    Java1st

---
+++ Anagram.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.*;
+
+public class PracticeDemo {
+    static boolean isAnangram(char s1[],char s2[]){
+        int l1=s1.length;
+        int l2=s2.length;
+        if (l1!=l2){
+            return false;
+
+        }
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        for(int i=0;i<l1;i++) {
+            if (s1[i] != s2[i]) {
+                return false;
+            }
+        }
+            return true;
+
+
+
+    }
+    public static void main(String args[]){
+        char str1[]="silent".toCharArray();
+        char str2[]="listen".toCharArray();
+        if(isAnangram(str1,str2)){
+            System.out.println("Anagram");
+        }
+        else{
+            System.out.println("Not a anagram");
+        }
+
+    }
+
+
+}
